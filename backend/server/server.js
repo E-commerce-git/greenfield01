ed1eacae82b14a229f856448bccb0e4f6c6e51b5
fahmi-najjari <fahminajjari@outlook.com>
2025-01-29 09:19:33 +0100

    auth backend

--- backend/server/server.js
+++ backend/server/server.js
@@ -1,22 +1,36 @@
 // import express (after npm install express)
 const express = require('express');
 const PORT = 3000;
+const helmet = require('helmet')
 const app = express();
+const sequelize = require('./config/database')
 const cors=require("cors")
-
-
+const authRoutes = require('./routes/authRoutes');
+require('dotenv').config()
+app.use(helmet())
 
 const database=require("../database/connection")
 
 app.use(express.json());
 app.use(cors())
-// server configuration
 
 
+app.use('/api/auth', authRoutes)
 
-// make the server listen to requests
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).json({ message: 'Something went wrong!' });
+});
 app.listen(PORT, () => {
   console.log(`Server running at: http://localhost:${PORT}/`);
 });
 
 module.exports=app
+
+
+
+
+
+
+
+
